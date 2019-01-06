a5da475d33ff3ae5066de4f057ee84ba5a904b60
misko <misko@miskos-imac.local>
2019-01-06 13:38:29 +0100

    Initial Commit

---
+++ HotTracks/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HotTracks
+//
+//  Created by misko on 06/01/2019.
+//  Copyright © 2019 FMFI UK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
